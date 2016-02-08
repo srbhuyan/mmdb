50c268cc1893b23993a05b619483f07c7161a4b1
Soumya Ranjan Bhuyan <soumya.tiklu@gmail.com>
2016-02-07 18:29:28 -0700

    Basic gridview and ImageAdapter

--- app/src/main/java/com/lukti/android/mmdb/mobilemoviedatabase/MainActivityFragment.java
+++ app/src/main/java/com/lukti/android/mmdb/mobilemoviedatabase/MainActivityFragment.java
@@ -1,15 +1,21 @@
 package com.lukti.android.mmdb.mobilemoviedatabase;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import com.lukti.android.mmdb.mobilemoviedatabase.adapter.ImageAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mMovieAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +23,11 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridView = (GridView)rootView;
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        return rootView;
     }
 }
